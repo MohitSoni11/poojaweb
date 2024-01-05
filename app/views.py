3e518b65909e6fa57a7377988f49ea12c64cd4dd
Mohit Soni <mohitksoni1104@outlook.com>
2024-01-04 20:28:36 -0800

    Adding more views

--- app/views.py
+++ app/views.py
@@ -3,4 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-  return render(request, 'app/home.html', {})
+  return render(request, 'app/home.html')
+
+def login(request):
+  return render(request, 'app/login.html')
+
+def signup(request):
+  return render(request, 'app/signup.html')
